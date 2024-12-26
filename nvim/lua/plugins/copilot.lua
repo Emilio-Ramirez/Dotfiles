c79fbd2184b0f5eea93aab5101572de8dd7f090d
Emilio Ramírez Mascarúa <ramarm0825@gmail.com>
2024-12-26 15:03:07 -0600

    Updating copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,12 +2,25 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   build = ":Copilot auth",
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
-    suggestion = { enabled = true },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = "<Tab>",      -- Tab to accept
+        next = "<C-n>",       -- Ctrl+n to get next suggestion
+        prev = "<C-p>",       -- Ctrl+p to get previous suggestion
+        dismiss = "<C-e>",    -- Ctrl+e to dismiss suggestion
+      },
+    },
     panel = { enabled = true },
     filetypes = {
       markdown = true,
       help = true,
+      javascript = true,
+      ["*"] = true,
     },
   },
 }
